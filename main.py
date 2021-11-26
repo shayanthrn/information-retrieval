714c330b37ac91a1c301878b40424778e9529eed
hassan kazemi tehrani <shayanthrn@gmail.com>
2021-11-26 20:39:44 +0330

    final changes to process a query

--- main.py
+++ main.py
@@ -48,8 +48,11 @@ class Positional_index():
                 self.indexes[token] = {'count':len(token_indexes), 'postings':{id:{'count':len(token_indexes),'positions':token_indexes}}}
     
     def find(self,token):
+        result = {}
         if(token in self.indexes.keys()):
-            return list(self.indexes[token]['postings'].keys())
+            for key in self.indexes[token]['postings'].keys():
+                result[key] = self.indexes[token]['postings'][key]['positions']
+            return result
         return None
     
 
@@ -85,6 +88,48 @@ if __name__ == '__main__':
         for j in range(len(tokens)):
             tokens[j] = preprocessor.lemmetizer_and_stemmer(tokens[j])
         if(len(tokens)==1):
-            print(positional_index.find(tokens[0]))
+            ndict= positional_index.find(tokens[0])
+            if(ndict == None):
+                print("No result found")
+                continue
+            most_related_id = list(ndict.keys())[0]
+            print("Id of most related document is ",most_related_id," and its title is :\n")
+            print(positional_index.titles[most_related_id])
         else:
-            print(tokens[0])
+            docIDs = []
+            docIDs_l = []
+            priorities = []
+            for token in tokens:
+                docIDs.append(positional_index.find(token))
+            if None in docIDs:
+                print("No result found")
+                continue
+            for dic in docIDs:
+                docIDs_l.append(list(dic.keys()))
+            while(len(docIDs_l)>0):
+                priorities.append(set.intersection(*map(set,docIDs_l)))
+                docIDs_l.pop()
+            for i in range(1,len(priorities)):
+                priorities[i] = priorities[i].difference(priorities[i-1])
+            final_list = []
+            
+            for myset in priorities:
+                temp_list=[]
+                for id in list(myset):
+                    score = 0
+                    for j in range(len(docIDs)-1):
+                        if(id in docIDs[j].keys() and id in docIDs[j+1].keys()):
+                            for position in docIDs[j][id]:
+                                if(position+1 in docIDs[j+1][id]):
+                                    score += 1
+                    temp_list.append((id,score))
+                temp_list=sorted(temp_list, key=lambda x: x[1],reverse=True)
+                final_list += temp_list
+            
+            most_related_id = final_list[0][0]
+            print("Id of most related document is ",most_related_id," and its title is :\n")
+            print(positional_index.titles[most_related_id])
+
+
+                
+
