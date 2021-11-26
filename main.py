473d986704a9bca636d5514ca5fa6c3a5b24ed79
hassan kazemi tehrani <shayanthrn@gmail.com>
2021-11-26 17:56:26 +0330

    searching query

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 from hazm import *
 import pandas as pd
+import pickle
+import os
 
 
 
@@ -11,6 +13,7 @@ class Preprocessor():
         self.stemmer = Stemmer()
         self.lemmatizer = Lemmatizer() 
         self.stopwords_set = set(stopwords_list())
+        self.stopwords_set.update(['.',':','?','!','/','//','*','**','***','[',']','{','}',';','\'','\"','(',')',''])
         
     def filter_stopwords(self,input_tokens):
         return [token for token in input_tokens if token not in self.stopwords_set]
@@ -23,8 +26,10 @@ class Preprocessor():
     
     def lemmetizer_and_stemmer(self,token):
         res = self.lemmatizer.lemmatize(token)
-        res = self.stemmer.stem(res)
+        # res = self.stemmer.stem(res)
         return res
+
+
 
 class Positional_index():
     def __init__(self,dataframe):
@@ -34,15 +39,52 @@ class Positional_index():
             self.titles[i] = dataframe.loc[i].title
     
     def add_and_merge_content(self,tokens,id):
-        pass
-
+        for token in set(tokens):
+            token_indexes = [i for i,val in enumerate(tokens) if val==token]
+            if(token in self.indexes.keys()):
+                self.indexes[token]['count'] += len(token_indexes)
+                self.indexes[token]['postings'][id] = {'count':len(token_indexes),'positions':token_indexes}
+            else:
+                self.indexes[token] = {'count':len(token_indexes), 'postings':{id:{'count':len(token_indexes),'positions':token_indexes}}}
+    
+    def find(self,token):
+        if(token in self.indexes.keys()):
+            return list(self.indexes[token]['postings'].keys())
+        return None
+    
 
 if __name__ == '__main__':
-    dataset = pd.read_excel("C:\\university\\Information Retrival\\Project\\information-retrieval\\IR1_7k_news.xlsx")
     preprocessor = Preprocessor()
-    positional_index = Positional_index(dataset)
-    for i in range(len(dataset)):
-        normalized_content = preprocessor.fnormilizer(dataset.loc[i].content)
-        tokens = preprocessor.tokenizer(normalized_content)
+
+    if(os.path.isfile('positional_index.pickle')):
+        file_to_read = open("positional_index.pickle", "rb")
+        positional_index=pickle.load(file_to_read)
+    else:
+        dataset = pd.read_excel("C:\\university\\Information Retrival\\Project\\information-retrieval\\IR1_7k_news.xlsx")
+        positional_index = Positional_index(dataset)
+        for i in range(len(dataset)): #deploy phase
+        # for i in range(2):  #test phase
+            #preprocess
+            normalized_content = preprocessor.fnormilizer(dataset.loc[i].content)
+            tokens = preprocessor.tokenizer(normalized_content)
+            tokens = preprocessor.filter_stopwords(tokens)
+            for j in range(len(tokens)):
+                tokens[j] = preprocessor.lemmetizer_and_stemmer(tokens[j])
+            #indexing
+            positional_index.add_and_merge_content(tokens,i)
+        file_to_write = open("positional_index.pickle", "wb")
+        pickle.dump(positional_index, file_to_write)
+
+    while(True):
+        query = input("Enter your Query: (enter exit to exit from the program)\n")
+        if(query.lower()=="exit"):
+            os._exit(0)
+        normlized = preprocessor.fnormilizer(query)
+        tokens = preprocessor.tokenizer(normlized)
         tokens = preprocessor.filter_stopwords(tokens)
-        for token in tokens:
+        for j in range(len(tokens)):
+            tokens[j] = preprocessor.lemmetizer_and_stemmer(tokens[j])
+        if(len(tokens)==1):
+            print(positional_index.find(tokens[0]))
+        else:
+            print(tokens[0])
