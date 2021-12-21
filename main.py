dfa0aea84a2fe76b096344faa2a3154278fdf410
hassan kazemi tehrani <shayanthrn@gmail.com>
2021-12-21 13:35:41 +0330

    finishing phase 2

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from hazm import *
 import pandas as pd
 import pickle
 import os
+import math
 
 
 
@@ -13,7 +14,7 @@ class Preprocessor():
         self.stemmer = Stemmer()
         self.lemmatizer = Lemmatizer() 
         self.stopwords_set = set(stopwords_list())
-        self.stopwords_set.update(['.',':','?','!','/','//','*','**','***','[',']','{','}',';','\'','\"','(',')','','،'])
+        self.stopwords_set.update(['.',':','?','!','/','//','*','**','***','[',']','{','}',';','\'','\"','(',')','','،',''])
         
         
     def filter_stopwords(self,input_tokens):
@@ -27,7 +28,7 @@ class Preprocessor():
     
     def lemmetizer_and_stemmer(self,token):
         res = self.lemmatizer.lemmatize(token)
-        # res = self.stemmer.stem(res)
+        res = self.stemmer.stem(res)
         return res
 
 
@@ -36,6 +37,7 @@ class Positional_index():
     def __init__(self,dataframe):
         self.indexes = {}
         self.titles = {}
+        self.squaroftfidfsize = [0 for a in range(len(dataframe))]
         for i in range(len(dataframe)):
             self.titles[i] = dataframe.loc[i].title
     
@@ -44,9 +46,9 @@ class Positional_index():
             token_indexes = [i for i,val in enumerate(tokens) if val==token]
             if(token in self.indexes.keys()):
                 self.indexes[token]['count'] += len(token_indexes)
-                self.indexes[token]['postings'][id] = {'count':len(token_indexes),'positions':token_indexes}
+                self.indexes[token]['postings'][id] = {'tf':len(token_indexes),'positions':token_indexes}
             else:
-                self.indexes[token] = {'count':len(token_indexes), 'postings':{id:{'count':len(token_indexes),'positions':token_indexes}}}
+                self.indexes[token] = {'count':len(token_indexes), 'postings':{id:{'tf':len(token_indexes),'positions':token_indexes}}}
     
     def find(self,token):
         result = {}
@@ -55,90 +57,121 @@ class Positional_index():
                 result[key] = self.indexes[token]['postings'][key]['positions']
             return result
         return None
+
+def make_championlist(positional_index):
+    for token in positional_index.indexes.keys():
+        championlist = []
+        postings = positional_index.indexes[token]['postings']
+        for docid in postings.keys():
+            championlist.append((docid,postings[docid]['tfidf']))
+        championlist= sorted(championlist, key=lambda x: x[1],reverse=True)
+        positional_index.indexes[token]['championlist'] = championlist[:40]   #40 best documents are in champion list
+        positional_index.indexes[token]['lowlist'] = championlist[40:]
+
+
+def calculate_idf(positional_index,N):
+    for key in positional_index.indexes.keys():
+        positional_index.indexes[key]['idf'] = math.log10(N/(len(positional_index.indexes[key]['postings'].keys()))) 
+        for docid in positional_index.indexes[key]['postings'].keys():
+            tfidf = positional_index.indexes[key]['idf']* (1+math.log10(positional_index.indexes[key]['postings'][docid]['tf']))
+            positional_index.indexes[key]['postings'][docid]['tfidf'] = tfidf
+            positional_index.squaroftfidfsize[docid] += tfidf**2
+
+def make_positional_index(dataset):
+    preprocessor = Preprocessor()
+    positional_index = Positional_index(dataset)
+    for i in range(len(dataset)): #deploy phase
+        #preprocess
+        normalized_content = preprocessor.fnormilizer(dataset.loc[i].content)
+        tokens = preprocessor.tokenizer(normalized_content)
+        tokens = preprocessor.filter_stopwords(tokens)
+        for j in range(len(tokens)):
+            tokens[j] = preprocessor.lemmetizer_and_stemmer(tokens[j])
+        #indexing
+        positional_index.add_and_merge_content(tokens,i)
+    calculate_idf(positional_index,len(dataset))
+    make_championlist(positional_index)
+    file_to_write = open("positional_index.pickle", "wb")
+    pickle.dump(positional_index, file_to_write)
+    return positional_index
+
+
     
 def main():
-    preprocessor = Preprocessor()
     dataset = pd.read_excel("C:\\university\\Information Retrival\\Project\\information-retrieval\\IR1_7k_news.xlsx")
     if(os.path.isfile('positional_index.pickle')):
         file_to_read = open("positional_index.pickle", "rb")
         positional_index=pickle.load(file_to_read)
     else:
-        positional_index = Positional_index(dataset)
-        for i in range(len(dataset)): #deploy phase
-        # for i in range(2):  #test phase
-            #preprocess
-            normalized_content = preprocessor.fnormilizer(dataset.loc[i].content)
-            tokens = preprocessor.tokenizer(normalized_content)
-            tokens = preprocessor.filter_stopwords(tokens)
-            for j in range(len(tokens)):
-                tokens[j] = preprocessor.lemmetizer_and_stemmer(tokens[j])
-            #indexing
-            positional_index.add_and_merge_content(tokens,i)
-        file_to_write = open("positional_index.pickle", "wb")
-        pickle.dump(positional_index, file_to_write)
+        positional_index= make_positional_index(dataset)
 
+    preprocessor = Preprocessor()
     while(True):
         query = input("Enter your Query: (enter exit to exit from the program)\n")
         if(query.lower()=="exit"):
             os._exit(0)
         normlized = preprocessor.fnormilizer(query)
-        tokens = preprocessor.tokenizer(normlized)
-        tokens = preprocessor.filter_stopwords(tokens)
-        for j in range(len(tokens)):
-            tokens[j] = preprocessor.lemmetizer_and_stemmer(tokens[j])
-        if(len(tokens)==1):
-            ndict= positional_index.find(tokens[0])
-            if(ndict == None):
-                print("No result found")
-                continue
-            most_related_id = list(ndict.keys())[0]
-            print("Id of most related document is ",most_related_id," and its title is :\n")
-            print(positional_index.titles[most_related_id])
-            sentences = sent_tokenize(dataset.loc[most_related_id].content)
-            print("related sentences:\n")
-            for sen in sentences:
-                if(tokens[0] in sen):
-                    print(sen)
+        qtokens = preprocessor.tokenizer(normlized)
+        qtokens = preprocessor.filter_stopwords(qtokens)
+        for j in range(len(qtokens)):
+            qtokens[j] = preprocessor.lemmetizer_and_stemmer(qtokens[j]) 
+        terms = {}
+        related_docs = {} #docid: dot product
+        for term in qtokens:
+            if(term in terms.keys()):
+                terms[term]+= 1
+            else:
+                terms[term] = 1
+        vectorsize_query = 0
+        
+        for term in terms.keys():
+            if(term in positional_index.indexes.keys()):
+                idf= positional_index.indexes[term]['idf']
+                query_term_tfidf = (1+math.log10(terms[term]))*idf
+                vectorsize_query += query_term_tfidf**2
+                for doc in positional_index.indexes[term]['championlist']:
+                    docid = doc[0]
+                    doc_tfidf = doc[1]
+                    if(docid in related_docs.keys()):
+                        related_docs[docid] += query_term_tfidf*doc_tfidf
+                    else:
+                        related_docs[docid] = query_term_tfidf*doc_tfidf
+
+        if(len(related_docs.keys())<5): #if didnt find k best in championlist k=5
+            for term in terms.keys():
+                if(term in positional_index.indexes.keys()):
+                    idf= positional_index.indexes[term]['idf']
+                    query_term_tfidf = (1+math.log10(terms[term]))*idf
+                    vectorsize_query += query_term_tfidf**2
+                    for doc in positional_index.indexes[term]['lowlist']:
+                        docid = doc[0]
+                        doc_tfidf = doc[1]
+                        if(docid in related_docs.keys()):
+                            related_docs[docid] += query_term_tfidf*doc_tfidf
+                        else:
+                            related_docs[docid] = query_term_tfidf*doc_tfidf
+
+
+        if(len(related_docs.keys())==0):
+            print("no result found")
         else:
-            docIDs = []
-            docIDs_l = []
-            priorities = []
-            for token in tokens:
-                docIDs.append(positional_index.find(token))
-            if None in docIDs:
-                print("No result found")
-                continue
-            for dic in docIDs:
-                docIDs_l.append(list(dic.keys()))
-            while(len(docIDs_l)>0):
-                priorities.append(set.intersection(*map(set,docIDs_l)))
-                docIDs_l.pop()
-            for i in range(1,len(priorities)):
-                priorities[i] = priorities[i].difference(priorities[i-1])
-            final_list = []
+            vectorsize_query = vectorsize_query**0.5
+            for docid in related_docs.keys():
+                related_docs[docid] /= positional_index.squaroftfidfsize[docid] **0.5
+                related_docs[docid] /= vectorsize_query
             
-            for myset in priorities:
-                temp_list=[]
-                for id in list(myset):
-                    score = 0
-                    for j in range(len(docIDs)-1):
-                        if(id in docIDs[j].keys() and id in docIDs[j+1].keys()):
-                            for position in docIDs[j][id]:
-                                if(position+1 in docIDs[j+1][id]):
-                                    score += 1
-                    temp_list.append((id,score))
-                temp_list=sorted(temp_list, key=lambda x: x[1],reverse=True)
-                final_list += temp_list
-            
-            most_related_id = final_list[0][0]
-            print("Id of most related document is ",most_related_id," and its title is :\n")
-            print(positional_index.titles[most_related_id])
-            sentences = sent_tokenize(dataset.loc[most_related_id].content)
+            related_docs_tuple = [(k, v) for k, v in related_docs.items()]
+            related_docs_tuple=sorted(related_docs_tuple, key=lambda x: x[1],reverse=True)
+            print("5 Best document ID with their similarity score:")
+            print(related_docs_tuple[:5])
+            print("title of most related doc:")
+            print(positional_index.titles[related_docs_tuple[0][0]])
+            sentences = sent_tokenize(dataset.loc[related_docs_tuple[0][0]].content)
             print("related sentences:\n")
             for sen in sentences:
-                if(tokens[0] in sen):
-                    print(sen)    
-    
+                if(qtokens[0] in sen):
+                    print(sen)
+
 def check_zipfs_law():
     preprocessor = Preprocessor()
     dataset = pd.read_excel("C:\\university\\Information Retrival\\Project\\information-retrieval\\IR1_7k_news.xlsx")
@@ -179,7 +212,5 @@ def check_heaps_law():
     print("sum for tokens for ",count," document is: ",sum," and length of vocabulary(M) is : ", len(list(positional_index.indexes.keys())))
 
 if __name__ == '__main__':
-    # main()
-    # check_zipfs_law()
-    check_heaps_law()            
+    main()        
 
